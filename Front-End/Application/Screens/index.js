e916ff74bae2df3ae0b28f1a4ad63792fce23093
lagoonie <79497735+lagoonie@users.noreply.github.com>
2022-02-19 20:58:16 -0800

    First commit retry

---
+++ Front-End/Application/Screens/index.js
@@ -0,0 +1,11 @@
+ import Home from './Home'
+ import ImageScan from './ImageScan'
+ import TextScan from './TextScan'
+ import PdfScan from './PdfScan'
+
+ export {
+   Home,
+   ImageScan,
+   TextScan,
+   PdfScan
+ }
